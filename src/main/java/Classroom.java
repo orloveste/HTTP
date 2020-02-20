0eecd4f56ed9ba975e6fc8788ab73c6d859dd56c
orloveste <orlovevm@yahoo.com>
2020-02-20 08:44:16 +0200

    http tools and exemple

--- src/main/java/Classroom.java
+++ src/main/java/Classroom.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Classroom {
+    public Classroom(){
+    }
+
+    public Classroom(String classRoomName){
+        this.classRoomName = classRoomName;
+    }
+
+    public List<Student> studentList = new ArrayList<>();
+    public String classRoomName;
+
+    public void addStudent(Student student){
+
+    }
 }
